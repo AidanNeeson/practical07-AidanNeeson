7d21ba59de43ecaf959a4854035a2da424ecd051
Aidan Neeson <neeson01@allegheny.edu>
2022-12-09 21:57:58 -0500

    Parametrize the first test

--- tests/test_tf_objectoriented.py
+++ tests/test_tf_objectoriented.py
@@ -1,17 +1,20 @@
 import pytest
 import tf_objectoriented
 
-# TODO: Parametrize this test
-def test_DataStorageManager_populates_data():
+@pytest.mark.parametrize(
+    "input_file,expected_length",
+    [("tests/inputs/cats.txt", 12), ("tests/inputs/dogs.txt", 13)]
+)
+def test_DataStorageManager_populates_data(input_file, expected_length):
     # Given the input file
-    storage_manager = tf_objectoriented.DataStorageManager("tests/inputs/cats.txt")
+    storage_manager = tf_objectoriented.DataStorageManager(input_file)
 
     # When, words are accessed
     word_list = storage_manager.words()
 
     # Then, words are populated
     assert word_list is not None
-    assert len(word_list) == 12
+    assert len(word_list) == expected_length
 
 
 # TODO: Test the StopWordManager
